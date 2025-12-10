72bc1b2aa919ee1a328336c36da052af43711514
quantranvn <161212140+quantranvn@users.noreply.github.com>
2025-12-10 12:29:20 +0700

    Update AppStore.tsx

--- src/components/AppStore.tsx
+++ src/components/AppStore.tsx
@@ -542,14 +542,12 @@ export function AppStoreDialogContent({
 
         <TabsContent value="designer" className="mt-0 flex-1">
           <ScrollArea className="h-[85vh] pr-4">
-            <div className="mt-4 h-[85vh] overflow-hidden rounded-xl border shadow-inner">
                 <iframe
                   title="Animation designer toolkit"
                   src={animationToolkitUrl}
                   className="h-full w-full min-h-[600px] border-0 bg-background"
                   loading="lazy"
                 />
-            </div>
           </ScrollArea>
         </TabsContent>
 
